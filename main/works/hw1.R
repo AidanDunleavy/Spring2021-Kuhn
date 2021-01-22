e3c4320b9d416c84c081e982844ea37bab8f7118
Hammey <aidandunleavy00@gmail.com>
2021-01-21 19:54:10 -0600

    added test1 file

--- main/works/hw1.R
+++ main/works/hw1.R
@@ -30,6 +30,7 @@ par(mfrow=c(2,2))
 plot(res.aov)
 par(mfrow = c(1,1))
 data$Group
+
 plot.new()
 plot(c("4", "4.75", "5.5", "6"), by(data$time, data$Group, mean),
      col = c("#00AFBB", "#E7B800", "#FC4E07", "#00bb57"))
@@ -37,4 +38,3 @@ plot(c("4", "4.75", "5.5", "6"), by(data$time, data$Group, mean),
 plot(data$Group, data$time)
 model = lm(time ~ Group, data = data)
 summary(model)
-
